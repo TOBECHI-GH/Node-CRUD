9c8498e974b185b1520623f8962311bbddab78c7
Tobechukwu Onwuazombe <tobechi300@gmail.com>
2021-05-13 08:35:13 +0100

    schema

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = require('express')();
 // const { MongoClient } = mongodb;
 
 const mongoose = require('mongoose');
+const { getMaxListeners } = require('node:process');
 
 //connection string usually has a port that the database is listening on (27017)
 const connectionString = 'mongodb://localhost:27017/Biodata';
@@ -46,5 +47,25 @@ mongoose.connect(connectionString,
 //  .catch(err => {
 //      console.log(err)
 //  })
+
+const biodataSchema = new Schema({
+    message: String,
+    data: {
+        name: String,
+        email: String,
+        country: String
+    }
+});
+
+const Bio = mongoose.model('Bio', biodataSchema); 
+
+Bio.create({
+    message: "request was successful",
+    data: {
+        name: Tobe,
+        email: xyz,
+        country: Nigeria
+    }
+})
 
 app.listen(5000, () => console.log('app connected'))
